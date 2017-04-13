7b8b4d81ab26e78b14bebcbdddb72f9652092825
Alex Fosco <alex.hf88@gmail.com>
2017-04-13 11:14:54 -0600

    used postman to hit api, added connection to service

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -2,6 +2,6 @@ class BestbuyService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new()
+    @connection = Faraday.new("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,storeType,city,distance,phone&pageSize=10&apiKey=#{Figaro.env.bestbuy_key}")
   end
 end
