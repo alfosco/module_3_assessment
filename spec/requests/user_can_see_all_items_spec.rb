adc401cbdf920c196773f52c385ddb22134fea87
Alex Fosco <alex.hf88@gmail.com>
2017-04-13 09:35:54 -0600

    adds test to get items and deletes factory girl

---
+++ spec/requests/user_can_see_all_items_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "User" do
+    it " sends a GET request to /api/v1/items and receives a 200 JSON response containing all items" do
+      item_1 = Item.create!(name: "Laser Disk", description: "The best of the best.", image_url: "qwerty")
+      item_2 = Item.create!(name: "VCR", description: "You know you want one.", image_url: "asdfgh")
+      item_3 = Item.create!(name: "Gameboy", description: "Its awesome.", image_url: "zxcvbn")
+
+      visit '/api/v1/items'
+
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+
+      all_items = JSON.parse(response.body)
+
+      expect(all_items).to be_an(Array)
+      expect(all_items.count).to eq(3)
+    end
+end
--- spec/support/factories/item.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :item do
-    name Faker::Commerce.product_name
-    description Faker::Lorem.sentence
-    image_url Faker::Lorem.sentence
-  end
-end
